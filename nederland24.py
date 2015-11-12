b94fa77f74a6fe828061a30fef414c3f640648a6
bosel <boselbosel@gmail.com>
2015-11-12 22:15:41 +0100

    cleanup

--- nederland24.py
+++ nederland24.py
@@ -34,7 +34,7 @@ addonId = addon.getAddonInfo('id')
 pluginhandle = int(sys.argv[1])
 settings = xbmcaddon.Addon(id='plugin.video.nederland24')
 xbmcplugin.setContent(pluginhandle, 'episodes')
-#xbmcplugin.addSortMethod(pluginhandle, xbmcplugin.SORT_METHOD_EPISODE)  #enable for alphabetic listing
+# xbmcplugin.addSortMethod(pluginhandle, xbmcplugin.SORT_METHOD_EPISODE)  #enable for alphabetic listing
 IMG_DIR = os.path.join(settings.getAddonInfo("path"), "resources", "media")
 
 ###
@@ -47,62 +47,64 @@ TOKEN_URL = 'http://ida.omroep.nl/npoplayer/i.js'
 
 CHANNELS = [
   
-  ["NPO 1","npo_1.png","tvlive/ned1/ned1.isml/ned1.m3u8","Televisiekijken begint op NPO 1. Van nieuws en actualiteiten tot consumentenprogramma's en kwaliteitsdrama. Programma's die over jou en jouw wereld gaan. Met verhalen die je herkent over mensen die zomaar in je straat kunnen wonen. Ook als er iets belangrijks gebeurt, in Nederland of in de wereld, kijk je NPO 1."],
-  ["NPO 2","npo_2.png","tvlive/ned2/ned2.isml/ned2.m3u8","NPO 2 zet je aan het denken. Met programma's die verdiepen en inspireren. Als je wilt weten wat het verhaal achter de actualiteit is. Of als je het eens van een andere kant wilt bekijken. NPO 2 biedt het mooiste van Nederlandse en internationale kunst en cultuur, literatuur, documentaires, art-house films en kwaliteitsdrama."],
-  ["NPO 3","npo_3.png","tvlive/ned3/ned3.isml/ned3.m3u8","Op NPO 3 vind je programma's waar jong Nederland zich in herkent en die je uitdagen een eigen mening te vormen. Met veel aandacht voor nieuwe media en experimentele vernieuwing brengt NPO 3 een gevarieerd aanbod van de dagelijkse actualiteit tot muziek, reizen, human interest, talkshows en documentaires."],
+  ["NPO 1", "npo_1.png", "tvlive/ned1/ned1.isml/ned1.m3u8", "Televisiekijken begint op NPO 1. Van nieuws en actualiteiten tot consumentenprogramma's en kwaliteitsdrama. Programma's die over jou en jouw wereld gaan. Met verhalen die je herkent over mensen die zomaar in je straat kunnen wonen. Ook als er iets belangrijks gebeurt, in Nederland of in de wereld, kijk je NPO 1."],
+  ["NPO 2", "npo_2.png", "tvlive/ned2/ned2.isml/ned2.m3u8", "NPO 2 zet je aan het denken. Met programma's die verdiepen en inspireren. Als je wilt weten wat het verhaal achter de actualiteit is. Of als je het eens van een andere kant wilt bekijken. NPO 2 biedt het mooiste van Nederlandse en internationale kunst en cultuur, literatuur, documentaires, art-house films en kwaliteitsdrama."],
+  ["NPO 3", "npo_3.png", "tvlive/ned3/ned3.isml/ned3.m3u8", "Op NPO 3 vind je programma's waar jong Nederland zich in herkent en die je uitdagen een eigen mening te vormen. Met veel aandacht voor nieuwe media en experimentele vernieuwing brengt NPO 3 een gevarieerd aanbod van de dagelijkse actualiteit tot muziek, reizen, human interest, talkshows en documentaires."],
   ["NPO 101", "npo_101.png", "thematv/101tv/101tv.isml/101tv.m3u8", "Weg met suffe en saaie tv! Het is tijd voor NPO 101, het 24-uurs jongerenkanaal van BNN en de Publieke Omroep. Met rauwe en brutale programma's, van en voor jongeren. Boordevol hilarische fragmenten, spannende livegames, bizarre experimenten en nieuws over festivals en gratis concertkaartjes. Kijken dus!"],
-  ["NPO Best","npo_best.png","thematv/best24/best24.isml/best24.m3u8","NPO Best brengt hoogtepunten uit ruim zestig jaar Nederlandse televisiehistorie. Het is een feelgoodzender waarop u 24 uur per dag de mooiste programma's uit de schatkamer van de Publieke Omroep kunt zien."],
-  ["NPO Cultura","npo_cultura.png","thematv/cultura24/cultura24.isml/cultura24.m3u8","NPO Cultura is het digitale themakanaal van de Publieke Omroep voor verdieping in kunst en cultuur. 24 uur per dag programma's uit genres als klassiek, literatuur, dans, theater, pop, jazz, film, drama en beeldende kunst."],
-  ["NPO Zapp Xtra","npo_zapp.png","thematv/zappelin24/zappelin24.isml/zappelin24.m3u8","Zappelin Xtra en Zapp Xtra zendt dagelijks, 24 uur per dag en reclamevrij, de beste kinderprogramma's van de publieke omroep uit. Aansluitend op Nederland 3 zendt het themakanaal programma's uit van Zappelin of Zapp. Is op Nederland 3 iets voor kleuters te zien dan richt het themakanaal zich op oudere kinderen, en andersom."],
-  ["NPO Doc","npo_doc.png","thematv/hollanddoc24/hollanddoc24.isml/hollanddoc24.m3u8","NPO Doc is het documentaireplatform van de NPO en verrast je continu met de beste documentaires uit binnen- en buitenland en exclusieve interviews met regisseurs en topwetenschappers."],
-  ["NPO Humor TV","npo_humor_tv.png","thematv/humor24/humor24.isml/humor24.m3u8","NPO Humor TV is een uitgesproken comedykanaal: een frisse, Nederlandse humorzender met hoogwaardige, grappige, scherpe, jonge, nieuwe, satirische, humoristische programma's."],
-  ["NPO Nieuws","npo_nieuws.png","thematv/journaal24/journaal24.isml/journaal24.m3u8","Via het themakanaal 'NPO Nieuws' kunnen de live televisieuitzendingen van het NOS Journaal worden gevolgd. De laatste Journaaluitzending wordt herhaald tot de volgende uitzending van het NOS Journaal."],
-  ["NPO Politiek","npo_politiek.png","thematv/politiek24/politiek24.isml/politiek24.m3u8","NPO Politiek is het digitale kanaal over de Nederlandse politiek in de breedste zin van het woord."],
- ["NPO Radio 1","npo_radio1.png","visualradio/radio1/radio1.isml/radio1.m3u8","De onafhankelijke nieuws- en sportzender. Als er iets belangrijks gebeurt, in Nederland of in de wereld, luister je NPO Radio 1. Voor de achtergronden en het nieuws van alle kanten. Ook jouw mening telt. Er is veel ruimte voor opinie en debat waar ook luisteraars steevast aan deelnemen."],
-  ["NPO Radio 2","npo_radio2.png","visualradio/radio2/radio2.isml/radio2.m3u8","Informatie, actualiteit en het beste uit vijftig jaar popmuziek. Een toegankelijke zender met veel aandacht voor het Nederlandse lied, kleinkunst en cabaret."],
-  ["NPO 3FM","npo_3fm.png","visualradio/3fm/3fm.isml/3fm.m3u8","Op NPO 3FM staat de liefde voor muziek centraal. Samen met de luisteraar vindt NPO 3FM nieuwe muziek, nieuw Nederlands poptalent en jong radiotalent. Je komt onze dj's vaak tegen op festivals en concerten."],
-  ["NPO Radio 4", "npo_radio4.png", "visualradio/radio4/radio4.isml/radio4.m3u8", "De klassieke muziekzender voor zowel de ervaren als de nieuwe liefhebber. Naast de mooiste klassieke muziek, brengt NPO Radio 4 jaarlijks ongeveer twaalfhonderd concerten uit 's werelds beroemdste concertzalen. Waaronder drie eigen concertseries."], 
+  ["NPO Best", "npo_best.png", "thematv/best24/best24.isml/best24.m3u8", "NPO Best brengt hoogtepunten uit ruim zestig jaar Nederlandse televisiehistorie. Het is een feelgoodzender waarop u 24 uur per dag de mooiste programma's uit de schatkamer van de Publieke Omroep kunt zien."],
+  ["NPO Cultura", "npo_cultura.png", "thematv/cultura24/cultura24.isml/cultura24.m3u8", "NPO Cultura is het digitale themakanaal van de Publieke Omroep voor verdieping in kunst en cultuur. 24 uur per dag programma's uit genres als klassiek, literatuur, dans, theater, pop, jazz, film, drama en beeldende kunst."],
+  ["NPO Zapp Xtra", "npo_zapp.png", "thematv/zappelin24/zappelin24.isml/zappelin24.m3u8", "Zappelin Xtra en Zapp Xtra zendt dagelijks, 24 uur per dag en reclamevrij, de beste kinderprogramma's van de publieke omroep uit. Aansluitend op Nederland 3 zendt het themakanaal programma's uit van Zappelin of Zapp. Is op Nederland 3 iets voor kleuters te zien dan richt het themakanaal zich op oudere kinderen, en andersom."],
+  ["NPO Doc", "npo_doc.png", "thematv/hollanddoc24/hollanddoc24.isml/hollanddoc24.m3u8", "NPO Doc is het documentaireplatform van de NPO en verrast je continu met de beste documentaires uit binnen- en buitenland en exclusieve interviews met regisseurs en topwetenschappers."],
+  ["NPO Humor TV", "npo_humor_tv.png", "thematv/humor24/humor24.isml/humor24.m3u8", "NPO Humor TV is een uitgesproken comedykanaal: een frisse, Nederlandse humorzender met hoogwaardige, grappige, scherpe, jonge, nieuwe, satirische, humoristische programma's."],
+  ["NPO Nieuws", "npo_nieuws.png", "thematv/journaal24/journaal24.isml/journaal24.m3u8", "Via het themakanaal 'NPO Nieuws' kunnen de live televisieuitzendingen van het NOS Journaal worden gevolgd. De laatste Journaaluitzending wordt herhaald tot de volgende uitzending van het NOS Journaal."],
+  ["NPO Politiek", "npo_politiek.png", "thematv/politiek24/politiek24.isml/politiek24.m3u8", "NPO Politiek is het digitale kanaal over de Nederlandse politiek in de breedste zin van het woord."],
+  ["NPO Radio 1", "npo_radio1.png", "visualradio/radio1/radio1.isml/radio1.m3u8", "De onafhankelijke nieuws- en sportzender. Als er iets belangrijks gebeurt, in Nederland of in de wereld, luister je NPO Radio 1. Voor de achtergronden en het nieuws van alle kanten. Ook jouw mening telt. Er is veel ruimte voor opinie en debat waar ook luisteraars steevast aan deelnemen."],
+  ["NPO Radio 2", "npo_radio2.png", "visualradio/radio2/radio2.isml/radio2.m3u8", "Informatie, actualiteit en het beste uit vijftig jaar popmuziek. Een toegankelijke zender met veel aandacht voor het Nederlandse lied, kleinkunst en cabaret."],
+  ["NPO 3FM", "npo_3fm.png", "visualradio/3fm/3fm.isml/3fm.m3u8", "Op NPO 3FM staat de liefde voor muziek centraal. Samen met de luisteraar vindt NPO 3FM nieuwe muziek, nieuw Nederlands poptalent en jong radiotalent. Je komt onze dj's vaak tegen op festivals en concerten."],
+  ["NPO Radio 4", "npo_radio4.png", "visualradio/radio4/radio4.isml/radio4.m3u8", "De klassieke muziekzender voor zowel de ervaren als de nieuwe liefhebber. Naast de mooiste klassieke muziek, brengt NPO Radio 4 jaarlijks ongeveer twaalfhonderd concerten uit 's werelds beroemdste concertzalen. Waaronder drie eigen concertseries."],
   ["NPO Radio 5", "npo_radio5.png", "tvlive/mcr1/mcr1.isml/mcr1.m3u8", "Overdag is NPO Radio 5 een sfeervolle zender met vooral evergreens uit de jaren ’60 en ’70. In de avond en het weekeinde brengt NPO Radio 5 beschouwende en informatieve programma’s over: godsdienst, levensbeschouwing en specifieke (sub)culturen."],
-  ["NPO Radio 6", "npo_radio6.png", "visualradio/radio6/radio6.isml/radio6.m3u8", "De Soul & Jazz zender, met muziek van Miles Davis tot Caro Emerald. Onze dj’s en muzikanten nemen je mee op een nationale en internationale ontdekkingstocht. Daarnaast doet NPO Radio 6 jaarlijks verslag van festivals als North Sea Jazz."], 
+  ["NPO Radio 6", "npo_radio6.png", "visualradio/radio6/radio6.isml/radio6.m3u8", "De Soul & Jazz zender, met muziek van Miles Davis tot Caro Emerald. Onze dj’s en muzikanten nemen je mee op een nationale en internationale ontdekkingstocht. Daarnaast doet NPO Radio 6 jaarlijks verslag van festivals als North Sea Jazz."],
 ]
+
 
 def index():
     for channel in CHANNELS:
-        if settings.getSetting( channel[0] )=='true':
-            addLink(channel[0],channel[2], "playVideo", os.path.join(IMG_DIR, channel[1]), channel[3])
+        if settings.getSetting(channel[0]) == 'true':
+            addLink(channel[0], channel[2], "playVideo", os.path.join(IMG_DIR, channel[1]), channel[3])
         else:
             xbmc.log("plugin.video.nederland24:: %s not set" % str(channel[0]))
-    if int(settings.getSetting ( "Depth_Acht" ))!=0:
-        url='http://feeds.nos.nl/journaal20uur'
-        depth=int(settings.getSetting ( "Depth_Acht" ))
+    if int(settings.getSetting("Depth_Acht")) != 0:
+        url = 'http://feeds.nos.nl/journaal20uur'
+        depth = int(settings.getSetting("Depth_Acht"))
         additionalChannels(url, depth)
-    if int(settings.getSetting ( "Depth_Jeugd" ))!=0:
-        url='http://feeds.nos.nl/vodcast_jeugdjournaal'
-        depth=int(settings.getSetting ( "Depth_Jeugd" ))
+    if int(settings.getSetting("Depth_Jeugd")) != 0:
+        url = 'http://feeds.nos.nl/vodcast_jeugdjournaal'
+        depth = int(settings.getSetting("Depth_Jeugd"))
         additionalChannels(url, depth)
     else:
         xbmc.log("plugin.video.nederland24:: No additional channels set")
     xbmcplugin.endOfDirectory(pluginhandle)
 
+
 def prefer_clca():
-    if settings.getSetting( "CLCA" )=='true':
-    	for channel in CHANNELS:
-	    if channel[0]=="NPO 1":
-	    	channel[2] = "tvlive/npo1cc/npo1cc.isml/npo1cc.m3u8"
-	    elif channel[0]=="NPO 2":
-	    	channel[2] = "tvlive/npo2cc/npo2cc.isml/npo2cc.m3u8"
-	    elif channel[0]=="NPO 3":
-		channel[2] = "tvlive/npo3cc/npo3cc.isml/npo3cc.m3u8"
+    if settings.getSetting("CLCA") == 'true':
+        for channel in CHANNELS:
+            if channel[0] == "NPO 1":
+                channel[2] = "tvlive/npo1cc/npo1cc.isml/npo1cc.m3u8"
+            elif channel[0] == "NPO 2":
+                channel[2] = "tvlive/npo2cc/npo2cc.isml/npo2cc.m3u8"
+            elif channel[0] == "NPO 3":
+                channel[2] = "tvlive/npo3cc/npo3cc.isml/npo3cc.m3u8"
 
 
 def resolve_http_redirect(url, depth=0):
     if depth > 10:
         raise Exception("Redirected "+depth+" times, giving up.")
-    o = urlparse.urlparse(url,allow_fragments=True)
+    o = urlparse.urlparse(url, allow_fragments=True)
     conn = httplib.HTTPConnection(o.netloc)
     path = o.path
     if o.query:
-        path +='?'+o.query
+        path += '?'+o.query
     conn.request("HEAD", path)
     res = conn.getresponse()
     headers = dict(res.getheaders())
@@ -111,17 +113,19 @@ def resolve_http_redirect(url, depth=0):
     else:
         return url
 
+
 def extract_url(chan):
-    URL=API_URL+BASE_URL+(chan)
+    URL = API_URL+BASE_URL+(chan)
     req = urllib2.Request(URL)
     req.add_header('User-Agent', USER_AGENT)
     response = urllib2.urlopen(req)
     page = response.read()
     response.close()
-    videopre=re.search(r'http:(.*?)url',page).group()
-    prostream= (videopre.replace('\/', '/'))
+    videopre = re.search(r'http:(.*?)url', page).group()
+    prostream = (videopre.replace('\/', '/'))
     video = resolve_http_redirect(prostream, 3)
     return video
+
 
 def collect_token():
     req = urllib2.Request(TOKEN_URL)
@@ -129,20 +133,20 @@ def collect_token():
     response = urllib2.urlopen(req)
     page = response.read()
     response.close()
-    token = re.search(r'npoplayer.token = "(.*?)"',page).group(1)
-    #xbmc.log("plugin.video.nederland24:: oldtoken: %s" % token)
+    token = re.search(r'npoplayer.token = "(.*?)"', page).group(1)
+    # xbmc.log("plugin.video.nederland24:: oldtoken: %s" % token)
     # site change, token invalid, needs to be reordered. Thanks to rieter for figuring this out very quickly.
     first = -1
     last = -1
     for i in range(5, len(token) - 5, 1):
-	#xbmc.log("plugin.video.nederland24:: %s" % token[i])
+        # xbmc.log("plugin.video.nederland24:: %s" % token[i])
         if token[i].isdigit():
             if first < 0:
                 first = i
-                #xbmc.log("plugin.video.nederland24:: %s" % token[i])
+                # xbmc.log("plugin.video.nederland24:: %s" % token[i])
             elif last < 0:
                 last = i
-                #xbmc.log("plugin.video.nederland24:: %s" % token[i])
+                # xbmc.log("plugin.video.nederland24:: %s" % token[i])
                 break
 
     newtoken = list(token)
@@ -154,7 +158,7 @@ def collect_token():
     newtoken = ''.join(newtoken)
     # xbmc.log("plugin.video.nederland24:: newtoken: %s" % newtoken)
     return newtoken
-    
+
 
 def addLink(name, url, mode, iconimage, description):
     u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+urllib.quote_plus(mode)
@@ -170,12 +174,12 @@ def addLink(name, url, mode, iconimage, description):
     liz.setProperty('IsPlayable', 'true')
     ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz)
     return ok
-    
+
 
 def additionalChannels(url, depth):
     i = 0
     URL = url
-    #URL = 'http://feeds.nos.nl/journaal'
+    # URL = 'http://feeds.nos.nl/journaal'
     items = SoupStrainer('item')
     for tag in BeautifulStoneSoup(urllib2.urlopen(URL).read(), parseOnlyThese=items):
         title = tag.title.contents[0]
@@ -186,13 +190,14 @@ def additionalChannels(url, depth):
         if i == int(depth):
             break
 
+
 def playVideo(url):
     media = url
-    finalUrl=""
+    finalUrl = ""
     if media and media.startswith("http://"):
-        finalUrl=media
+        finalUrl = media
     else:
-        URL=API_URL+BASE_URL+media+"&token=%s" % collect_token()
+        URL = API_URL+BASE_URL+media+"&token=%s" % collect_token()
         xbmc.log("plugin.video.nederland24:: URL and token %s" % str(URL))
         req = urllib2.Request(URL)
         req.add_header('User-Agent', USER_AGENT)
@@ -200,12 +205,13 @@ def playVideo(url):
         response = urllib2.urlopen(req)
         page = response.read()
         response.close()
-        videopre=re.search(r'http:(.*?)url',page).group()
-        prostream= (videopre.replace('\/', '/'))
+        videopre = re.search(r'http:(.*?)url', page).group()
+        prostream = (videopre.replace('\/', '/'))
         finalUrl = resolve_http_redirect(prostream)
     if finalUrl:
         listitem = xbmcgui.ListItem(path=finalUrl)
         xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
+
 
 def parameters_string_to_dict(parameters):
     paramDict = {}
@@ -228,4 +234,3 @@ if mode == "playVideo":
 else:
     prefer_clca()
     index()
-
